1488067aa358ee70a3d45409b2460edb6fd0d68b
Antti Ahvenlampi <antti.ahvenlampi@futurice.com>
2018-06-01 13:32:04 +0300

    add failing test

--- Test.Api/Test.Api.Tests/UnitTest1.cs
+++ Test.Api/Test.Api.Tests/UnitTest1.cs
@@ -10,5 +10,11 @@ namespace Test.Api.Tests
         {
             Assert.IsTrue(true);
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(false);
+        }
     }
 }
